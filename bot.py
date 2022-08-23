469ae7f184388b8b9c2516c52da8e753871fd75e
Carlettos <kako3000@gmail.com>
2022-08-23 19:36:28 -0400

    ahora usa el sistema de /system

--- bot.py
+++ bot.py
@@ -1,12 +1,18 @@
 import discord 
 import responses
 
+common_prefix = "/"
+private_prefix = "?"
+prefixes = [common_prefix, private_prefix]
+
 async def send_message(message, user_message, is_private):
-    try:
-        response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+    #try:
+    response = responses.handle_response(user_message)
+    if response == None or response == "":
+        return
+    await message.author.send(response) if is_private else await message.channel.send(response)
+    #except Exception as e:
+    #    print(e)
 
 def run_discord_bot():
     token = open("token").read()
@@ -24,13 +30,11 @@ def run_discord_bot():
         if message.author == client.user:
             return
         user_message = str(message.content)
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-
+        if user_message[0] not in prefixes:
+            return
+        if user_message[0] == private_prefix:
+            await send_message(message, user_message[1:], is_private=True)
         else:
-            await send_message(message, user_message, is_private=False)
-
-
+            await send_message(message, user_message[1:], is_private=False)
     client.run(token)
 
--- responses.py
+++ responses.py
@@ -1,15 +1,8 @@
-import random
-import subprocess
-import re
+from util import decodeTime, extractParams, beautify, toCode, getOutput, defaultTime, helpMessage
+from system_commands import tryTo
 
-#Cluster status texts
-def queue_status():
-    x = subprocess.check_output(['squeue', '-l'])
-    return str(x.decode('utf-8'))
-
-def cluster_status():
-    x = subprocess.check_output(['sinfo'])
-    return str(x.decode('utf-8'))
+_list = ["list", "jobs", "squeue"]
+create = ["create"]
 
 #User class
 class user():
@@ -19,9 +12,7 @@ class user():
         self.name = name
     
     def show_jobs(self):
-        print(f'valor de self.name = {self.name}')
-        x = subprocess.check_output(['squeue', '-l', '-u', self.name])
-        return str(x.decode('utf-8'))
+        return getOutput(["squeue", "-l", "-u", self.name])
 
     def create_slurm(self, varargs: list) -> str:
         command = open("simple").read()
@@ -29,13 +20,13 @@ class user():
         NTASKS = "1"
         OUT = "srun"
         ERR = "srun"
-        TIME = "01:00:00"
+        TIME = defaultTime
         COMMAND = ""
         MODULES = []
         for i in range(len(varargs)):
             arg = varargs[i]
             if arg == "-t":
-                TIME = self.decode_time(varargs[i+1])
+                TIME = decodeTime(varargs[i+1])
                 pass
             elif arg == "-log":
                 OUT = varargs[i+1]
@@ -48,7 +39,7 @@ class user():
                 NAME = varargs[i+1]
                 pass
             elif arg == "-c":
-                COMMAND = self.get_command(i, varargs)
+                COMMAND = extractParams(i, varargs)
                 pass
             elif arg == "-modules" or arg == "-mod" or arg == "-m":
                 n = i + 1
@@ -63,80 +54,22 @@ class user():
         command = command.replace("ERR", ERR)
         command = command.replace("TIME", TIME)
         command = command.replace("command", COMMAND)
-        command = command.replace("MODULES", "\nmodule load ".join(MODULES))
-        return "> ```bash\n> " + command.replace("\n", "\n> ") + "```\n"
+        command = command.replace("MODULES", "\nmodule load ".join([""] + MODULES))
+        return beautify(toCode(command, "bash"))
+
+def handle_response(content: str) -> str:
+    message = content.lower().strip().split(" ")
+    n = len(message)
+    if n < 2:
+        if n == 1 and message[0] == "help":
+            return beautify(helpMessage)
     
-    def decode_time(self, time: str) -> str:
-        numero: re.Pattern = re.compile("[1-9]+")
-        days: re.Pattern = re.compile("[1-9][dD]")
-        hours: re.Pattern = re.compile("[1-9][0-9]{0,3}[hH]")
-        minutes: re.Pattern = re.compile("[1-9][0-9]{0,5}[mM]")
-        complete: re.Pattern = re.compile("([0-9]{1,2}:){2}[0-9]{2}")
-        if re.match(days, time) != None:
-            d = int(re.findall(numero, time)[0])
-            return str(d*24)+":00:00"
-        if re.match(hours, time) != None:
-            return str(h) + ":00:00"
-        if re.match(minutes, time) != None:
-            m = int(re.findall(numero, time)[0])
-            h = m//60
-            m = m%60
-            h_str = str(h)
-            m_str = str(m)
-            m_str = m_str if len(m_str) == 2 else "0" + m_str 
-            return h_str + ":" + m_str + ":00"
-        if re.match(complete, time) != None:
-            return time
-        return "01:00:00"
-
-    def get_command(self, i: int, varargs: list) -> str:
-        com = ""
-        n = i + 1
-        while n < len(varargs):
-            if varargs[n][0] == "-":
-                break
-            com += " " + varargs[n]
-            n += 1
-        return com
-
-def handle_response(message) -> str:
-    p_message = message.lower()
-
-    p_message = p_message.strip().split()
-
-    n = len(p_message)
-
-    #if name is given
-    name = None
-    if p_message[0][0] == "<":
-        name = p_message[0][1:-1]
-
-    #show queue status
-    if n == 1 and p_message[0] == 'queue':
-        msg = queue_status()
-        return str(msg)
-
-    #show cluster status
-    if n == 1 and p_message[0] == 'status':
-        msg = cluster_status()
-        return str(msg)
-
-    if n > 1 and name != None:
-        command = p_message[1]
-        u = user(name)
-        if command == 'list':
-            msg = u.show_jobs()
-            return msg
-        if command == "create":
-            return u.create_slurm(p_message[2:])
-        return f'usuario {name} y comando {command}'
-
-    if p_message[0] == 'hello':
-        return 'Hey There!'
-
-    if p_message[0] == 'roll':
-        return str(random.randint(1, 6))
-
-    if p_message[0] == "!help":
-        return "This is a help message."
-    return f'El mensaje {p_message} no prendio cabros'
+    if message[0] == "system":
+        return tryTo(message[1])
+    else:
+        u = user(message[0])
+        command = message[1]
+        if command in _list:
+            return toCode(u.show_jobs(), "hs")
+        if command in create:
+            return u.create_slurm(message[2:])
